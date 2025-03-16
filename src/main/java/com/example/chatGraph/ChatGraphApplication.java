b6e3f7f90d00222cf6ef997bb5c8a3787a5c6194
jkli51 <jkk307@naver.com>
2025-03-16 18:24:03 +0900

    Initial commit

---
+++ src/main/java/com/example/chatGraph/ChatGraphApplication.java
@@ -0,0 +1,13 @@
+package com.example.chatGraph;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatGraphApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatGraphApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/chatGraph/ChatGraphApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.chatGraph;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatGraphApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
